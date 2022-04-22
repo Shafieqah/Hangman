7b812764c1d5ad4a404be6389de3e05e9c710f12
Shafieqah <4270845@myuwc.ac.za>
2022-04-22 10:10:34 +0200

    Added code to hangman, to make the function better.

--- HangmanGameSln/HangmanGameSln/Hangman.Core/Game/Hangman.cs
+++ HangmanGameSln/HangmanGameSln/Hangman.Core/Game/Hangman.cs
@@ -6,7 +6,10 @@ namespace Hangman.Core.Game
     public class HangmanGame
     {
         private GallowsRenderer _renderer;
-
+        private string _guess;
+        private object mysteryWord;
+        private int _numberOfLives;
+        private string progress;
         public HangmanGame()
         {
             _renderer = new GallowsRenderer();
@@ -14,6 +17,9 @@ namespace Hangman.Core.Game
 
         public void Run()
         {
+
+            _numberOfLives = 6;
+
             _renderer.Render(5, 5, 6);
 
             Console.SetCursorPosition(0, 13);
@@ -26,6 +32,84 @@ namespace Hangman.Core.Game
 
             Console.Write("What is your next guess: ");
             var nextGuess = Console.ReadLine();
+
+            Random random = new Random();
+
+            string[] listwords = new string[10];
+            listwords[0] = "shafieqah";
+            listwords[1] = "mesha";
+            listwords[2] = "chauntina";
+            listwords[3] = "kayla";
+            listwords[4] = "angel";
+            listwords[5] = "lauren";
+            listwords[6] = "rhaadi";
+            listwords[7] = "taryn";
+            listwords[8] = "sima";
+            listwords[9] = "sam";
+
+            var index = random.Next(0, 20);
+
+            string mysteryWord = listwords[index];
+
+            char[] guess = mysteryWord.ToCharArray();
+
+
+            for (int i = 0; i < guess.Length; i++)
+            {
+
+                progress += "_";
+                Console.SetCursorPosition(0, 17);
+
+            }
+           
+            
+
+
+            while (_numberOfLives > 0)
+            {
+                _renderer.Render(5, 5, _numberOfLives);
+                Console.SetCursorPosition(0, 17);
+
+                char playerGuess = char.Parse(Console.ReadLine());
+
+
+                char[] ProgressArray = progress.ToCharArray();
+
+                bool correct = false;
+
+
+                bool v = 0 < guess.Length;
+
+                for (int i = 0; i < guess.Length; i++) 
+                {
+                    if (guess[i] == playerGuess)
+                    {
+                        ProgressArray[i] = guess[i];
+                        correct = true;
+                    }
+                }
+                progress = new string(ProgressArray);
+                Console.SetCursorPosition(0, 18);
+
+                Console.WriteLine(progress);
+
+                if (!correct)
+                {
+                    _numberOfLives--;
+                    _renderer.Render(5, 5, _numberOfLives);
+                }
+            }
+
+            Console.SetCursorPosition(2, 22);
+
+            if (_numberOfLives == 0)
+            {
+                Console.WriteLine($"game over");
+            }
+            else
+            {
+                Console.WriteLine($"you won with {_numberOfLives} left.");
+            }
         }
 
     }
